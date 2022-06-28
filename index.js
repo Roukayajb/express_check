202b931ba34672a8c8bb030367a582d8735b21fe
unknown <jbili.roukaya1@gmail.com>
2022-06-28 13:24:06 +0200

    update index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const Express= require('express')();
-const app=Express;
+const express= require('express');
+const app=express();
 function Auth() {
     const now = new Date()
     return now.getDay() !== 0 && now.getDay() !== 6 && now.getHours() >= 9 && now.getHours() < 17;
@@ -13,18 +13,18 @@ app.use(function(req, res, next){
         res.send('<h1 style="text-align: center;">This App is not available at Weekends !</h1>');
     }
  });
- //app.use(express.static('css'));
+ app.use(express.static('views'));
  app.get('/css/navstyle.css', function(req, res) {
-    res.sendFile(__dirname+'/css/navstyle.css');
-  });
-  app.get('/', function(req, res) {
-    res.sendFile(__dirname+'/views/acceuil.html');
-  });
-  app.get('/services', function(req, res) {
-    res.sendFile(__dirname+'/views/services.html');
-  });
-  app.get('/contact', function(req, res) {
-    res.sendFile(__dirname+'/views/contact.html');
+     res.sendFile(__dirname+'/css/navstyle.css');
+   });
+   app.get('/', function(req, res) {
+     res.sendFile(__dirname+'/views/acceuil.html');
+   });
+   app.get('/services', function(req, res) {
+     res.sendFile(__dirname+'/views/services.html');
+   });
+   app.get('/contact', function(req, res) {
+     res.sendFile(__dirname+'/views/contact.html');
   });
   
   
